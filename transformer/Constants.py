36dc73a2a43116e0ab66f79457418e78dbb3b731
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2024-05-11 02:47:10 +0900

    Create Constants.py

---
+++ transformer/Constants.py
@@ -0,0 +1,11 @@
+### Github[transformer.Constants.py]: https://github.com/ming024/FastSpeech2/blob/master/transformer/Constants.py
+
+PAD = 0
+UNK = 1
+BOS = 2
+EOS = 3
+
+PAD_WORD = "<blank>"
+UNK_WORD = "<unk>"
+BOS_WORD = "<s>"
+EOS_WORD = "</s>"
